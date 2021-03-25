f90873021f1a104f289e4c29af5a638bcd9f6cd5
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-03-25 17:44:26 -0300

    Create UserLogin.ts

---
+++ blogPessoal/src/app/model/UserLogin.ts
@@ -0,0 +1,10 @@
+export class UserLogin{
+    public id: number;
+    public email: string;
+    public senha: string;
+    public token: string;
+    public nome: string;
+    public url_foto: string;
+    public codigo_usuario: number;
+    public tipo: string;
+}   
