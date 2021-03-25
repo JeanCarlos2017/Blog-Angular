0ba3d590bb87acaf7c007b14b4f68ab67b53d2ee
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-03-25 14:09:16 -0300

    User.ts

---
+++ blogPessoal/src/app/model/User.ts
@@ -0,0 +1,9 @@
+export class User{
+    public id: number;
+    public email: string;
+    public senha: string;
+    public nome: string;
+    public url_foto: string;
+    public codigo_usuario: number;
+    public tipo: string;
+}
