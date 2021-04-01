b6284d81d1ac766fa1d136e64173e2e684ba00f1
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-01 16:17:52 -0300

    add rota de put e delete postagem

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
@@ -36,6 +38,14 @@ const routes: Routes = [
   {
     path: 'tema-delete/:id',
     component: TemaDeleteComponent
+  }, 
+  {
+    path: 'postagem-delete/:id',
+    component: PostagemDeleteComponent, 
+  }, 
+  {
+    path: 'postagem-edit/:id',
+    component: PostagemEditComponent
   }
 ];
 
